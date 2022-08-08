5d8c7605fb4a1482871c00a0b2a2a8dc25665704
Ablution-han220 <nhrthebest@163.com>
2022-08-07 20:25:10 -0400

    day23

---
+++ src/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // one pass binary search time:O(lgn) space:O(1)
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[low]) {
+                if (nums[mid] > target && nums[low] <= target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[high] >= target && nums[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
